bcf1557299fba208f05b9bf6ffddf408843fd060
Alejandra <alejandra.franco1290@gmail.com>
2023-12-11 20:28:54 -0500

    Registro e inicio de sesion

--- registro/iniciar_sesion.php
+++ registro/iniciar_sesion.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro De Usuario</title>
+    <title>Inicio De Sesión</title>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -16,7 +16,7 @@
 
 
     <!-- CSS -->
-    <link rel="stylesheet" href="./estilos/estilosRegistro.css">
+    <link rel="stylesheet" href="./estilos/estilosInicio_sesion.css">
 
     <!-- Iconos -->
     <script src="https://kit.fontawesome.com/1c7ea47b2b.js" crossorigin="anonymous"></script>
@@ -41,21 +41,7 @@
                 <div class="formulario col-md-6">
                     <form class="form" method="post">
                         <div class="titulo">
-                            <h1><strong> Crea tu cuenta</strong></h1>
-                        </div>
-                        <div class="parrafo">
-                            <p style="color: #FF9800;"> <strong> Estas a un paso de automatizar tu proceso de
-                                    selección</strong></p>
-                        </div>
-                        <div class="contenido mt-2">
-                            <div class="encabezado"></div>
-                            <label for="nombre"> Nombre</label>
-                            <input type="text" class="form-control" id="nombre" name="nombre" required>
-                        </div>
-                        <div class="contenido mt-2">
-                            <div class="encabezado"></div>
-                            <label for="cargo"> Cargo</label>
-                            <input type="text" class="form-control" id="cargo" name="cargo" required>
+                            <h1> <strong>¡Bienvenido de Nuevo!</strong></h1>
                         </div>
                         <div class="contenido mt-2">
                             <div class="encabezado"></div>
@@ -67,10 +53,17 @@
                             <label for="password"> Contraseña</label>
                             <input type="text" class="form-control" id="password" name="password" required>
                         </div>
+                        <div class="contenido mt-3 d-flex">
+                            <input type="checkbox" name="recordar" id="recordar" class="mr-3"><label for="recordar"
+                                style="color: #FF9800; margin-left:5px;"> Recordar</label>
+                            <a style="color: #FF9800; margin-left:95px;" href="olvidaste_contrasena.php"
+                                class="ml-auto">¿Olvidaste tu contraseña?</a>
 
-                        <button class="btn btn-primary col-12 mt-3"> Crear Cuenta</button>
+                        </div>
+
+                        <button class="btn btn-primary col-12 mt-3"> Ingresar </button>
                         <div class="m-3">
-                            ¿Ya tienes una cuenta? <a href="iniciar_sesion.php"  >Inicia Sesión aqui</a>
+                            ¿No tienes una cuenta? <a href="registrarse.php">Registrate aquí</a>
                         </div>
 
                     </form>
