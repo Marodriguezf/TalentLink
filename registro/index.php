3873ea36691ad9c6a32c8c10ca2371e541e14f89
Alejandra <alejandra.franco1290@gmail.com>
2023-12-11 21:14:04 -0500

    Estilos del menu y dashboard principal

---
+++ registro/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Fuentes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./estilos/estilosIndex.css">
+
+    <!-- Iconos -->
+    <script src="https://kit.fontawesome.com/1c7ea47b2b.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<!-- Menu lateral -->
+<div class="sidebar">
+    <h1 class="logo">TalentLink</h1>
+    <div class="menus">
+        <p>General</p>
+        <a href="index.php"><i class="iconos fa-solid fa-grip" name="dashboard-icon"></i>Dashboard</a>
+        <a href="index.php"><i class="iconos fa-solid fa-briefcase" name="vacante-icon"></i>Vacantes</a>
+        <a href="index.php"><i class="iconos fa-solid fa-user" name="candidato-icon"></i>Candidatos</a>
+        <p>Acciones</p>
+        <a href="index.php"><i class="iconos fa-solid fa-circle-plus"name="crear-icon"></i>Crear una vacante</a>
+        <a href="index.php"><i class="iconos fa-solid fa-user-plus"name="registrar-icon"></i>Registrar Candidato</a>
+        <p>Soporte</p>
+        <a href="index.php"><i class="iconos fa-solid fa-gear"name="setting-icon"></i>Configuración</a>
+        <a href="index.php"><i class="iconos fa-solid fa-circle-info"name="help-icon"></i>Soporte</a>
+
+
+
+    </div>
+
+</div>
+
+
+
+    <!-- Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- registro/registrarse.php
+++ registro/registrarse.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <!-- Fonts -->
+    <!-- Fuentes -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
@@ -70,7 +70,7 @@
 
                         <button class="btn btn-primary col-12 mt-3"> Crear Cuenta</button>
                         <div class="m-3">
-                            ¿Ya tienes una cuenta? <a href="iniciar_sesion.php"  >Inicia Sesión aqui</a>
+                            ¿Ya tienes una cuenta? <a href="iniciar_sesion.php">Inicia Sesión aqui</a>
                         </div>
 
                     </form>
